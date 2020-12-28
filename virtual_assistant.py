7bb45f40c16a2f2b9ef687b0c4d349929568bf29
CharlesJ10 <55907207+CharlesJ10@users.noreply.github.com>
2020-12-28 16:53:51 -0600

    Implement news function
    Include news function to get latest news and upgrade old functions

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -1,7 +1,6 @@
 # Description: This is a virtual assistant program that will greet you with a random greeting, get the date,
-# time, retrieve information on a person and perform basic calculations.
+# time, retrieve information from wikipedia, open videos on Youtube and read the latest news.
 
-# Resource Used: https://randerson112358.medium.com/build-a-virtual-assistant-using-python-2b0f78e68b94
 
 # Import the libraries
 import speech_recognition as sr 
@@ -15,7 +14,9 @@ import calendar
 import random 
 import wikipedia 
 import re 
+import pyttsx3
 import webbrowser
+from bs4 import BeautifulSoup as soup
 import urllib.request #used to make requests
 import urllib.parse #used to parse values into the url
 
@@ -39,7 +40,7 @@ def recordAudio():
     # Speech recognition using Google's Speech Recognition
     data = ''
     try: 
-        data = r.recognize_google(audio) # Google Speech Recognition converts audio to text.
+        data = r.recognize_google(audio).lower() # Google Speech Recognition converts audio to text.
         print('Hey, you said: ' + data)
     except sr.UnknownValueError:
         print('Google Speech Recognition could not understand')
@@ -49,23 +50,32 @@ def recordAudio():
     return data.lower()
 
 
-# Function to get the virtual assistant response
+# Function to get the virtual assistant response in speech
 def assistantResponse(text):
     print(text)
 
-    # Convert the text to speech
-    myObj = gTTS(text=text, lang='en', slow=False)
+    # # Convert the text to speech
+    # myObj = gTTS(text=text, lang='en', slow=False)
+    
+    # # Save the converted audio to a file
+    # myObj.save('assistant_response.mp3')    
 
-    # Save the converted audio to a file
-    myObj.save('assistant_response.mp3')    
+    # # Play the converted file and delete
+    # os.system('start assistant_response.mp3')
+    # os.remove('assistant_response.mp3')
 
-    # Play the converted file and delete
-    os.system('start assistant_response.mp3')
-    os.remove('assistant_response.mp3')
+    # Using python text to speech offline since there has been some problems with gTTS 
+    # (https://github.com/pndurette/gTTS/issues/232)
+    # (https://github.com/luoliyan/chinese-support-redux/issues/161#issuecomment-727697730)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
 
 # A function to check for wake word(s)
 def wakeWord(text):
-    WAKE_WORDS = ['hey charlie', 'okay charlie', 'hey computer', 'okay computer', 'charlie']
+    WAKE_WORDS = ['hey charlie', 'okay charlie', 'hey computer', 'okay computer', 'charlie', 'computer']
     text = text.lower() # Convert the text to lower case
 
     # Check to see if the users command/text contains a wake word
@@ -76,9 +86,10 @@ def wakeWord(text):
     # Result if wake word was not found
     return False
 
+# A function to get the current date 
 def getDate():
     now = datetime.datetime.now()
-    my_date = datetime.datetime.today
+    my_date = datetime.datetime.today()
     weekday = calendar.day_name[my_date.weekday()] # Returns weekdays e.g. Monday
     monthNum = now.month
     dayNum = now.day
@@ -92,6 +103,33 @@ def getDate():
     
     return 'Today is ' + weekday + ', ' + month_names[monthNum - 1] + ' the ' + ordinalNumbers[dayNum - 1] + '.'
 
+# A function to get the current timer 
+def getTime():
+    now = datetime.datetime.now()
+    meridiem = ''
+    if now.hour >= 12:
+        meridiem = 'p.m' # Post Meridiem (PM)
+        hour = now.hour - 12
+    else:
+        meridiem = 'a.m' # Ante Meridiem (AM)
+        hour = now.hour
+    
+    # Convert minute into a proper string e.g. '09' 
+    if now.minute < 10:
+        minute = '0' + str(now.minute)
+    else:
+        minute = str(now.minute)
+    
+    return 'Time is ' + str(hour) + ':' + minute + ' ' + meridiem + '. ' 
+
+
+# Function to retrieve headlines (titles) from rss feed and return them as list
+def getRSSHeadlines(rss_url):
+    Client = urllib.request.urlopen(rss_url)
+    xml_page = Client.read()
+    Client.close()
+    soup_page = soup(xml_page, "lxml") # Install lxml for parsing
+    return soup_page.findAll("item") # Returns all news items from the RSS news feed
 
 # Function to return random greeting response to user
 def greeting(text):
@@ -109,104 +147,83 @@ def greeting(text):
     # If no greeting was detected, then return an empty string
     return ''
 
-# Function to get a person first and last name
-def getPerson(text):
-    wordList = text.split() # Split text into a list of words
 
-    for i in range(0, len(wordList)):
-        if i + 3 <= len(wordList) - 1 and wordList[i].lower() == 'who' and wordList[i + 1].lower() == 'is':
-            return wordList[i + 2] + ' ' + wordList[i + 3]
+if __name__ == "__main__":
+        
+    while True:
+        # Record the audio
+        text = recordAudio()
 
-    
-while True:
-    # Record the audio
-    text = recordAudio()
-    response = '' # Empty response string initiially
-
-    # Checking for the wake word/phrase 
-    if (wakeWord(text) == True):
-        # Check for greetings by the user
-        response = response + greeting(text)
-
-        # Check to see if the user ask for date
-        if ('date' in text):
-            get_date = getDate()
-            response = response + ' ' + get_date
-
-        # Check to see if the user said time
-        elif ('time' in text):
-            now = datetime.datetime.now()
-            meridiem = ''
-            if now.hour >= 12:
-                meridiem = 'p.m' # Post Meridiem (PM)
-                hour = now.hour - 12
-            else:
-                meridiem = 'a.m' # Ante Meridiem (AM)
-                hour = now.hour
+        # Checking for the wake word/phrase 
+        if (wakeWord(text) == True):
+            # Check for greetings by the user
+            assistantResponse(greeting(text))
             
-            # Convert minute into a proper string e.g. '09' 
-            if now.minute < 10:
-                minute = '0' + str(now.minute)
-            else:
-                minute = str(now.minute)
+            # Charlie's Introduction
+            if ('your name' in text):
+                assistantResponse("I am Charlie, your Virtual Assistant!")
+
+            # Check to see if the user asked for date
+            elif ('date' in text):
+                get_date = getDate()
+                assistantResponse(get_date)
+
+            # Check to see if the user said time
+            elif ('time' in text):
+                get_time = getTime()
+                assistantResponse(get_time)
+
+            # Search Google
+            elif 'open google and search' in text:
+                reg_ex = re.search('open google and search (.*)', text)
+                search_for = text.split("search ", 1)[1] # Stores search keyword from the user
+                # For debugging
+                # print(search_for)
+
+                # Install Chrome driver for desired platform and place on system path
+                driver = webdriver.Chrome() 
+                driver.get('http://www.google.com')
+                search = driver.find_element_by_name('q') # Finds search box element
+                search.send_keys(str(search_for)) # Sends search keys
+                search.send_keys(Keys.RETURN) # Hits enter
+
+            # Open videos on YouTube
+            elif ('youtube' in text):
+                assistantResponse("Ok!")
+                reg_ex = re.search('youtube (.+)', text)
+                if (reg_ex):
+                    domain = text.split("search ", 1)[1]
+                    query_string = urllib.parse.urlencode({"search_query" : domain})
+                    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string) 
+                    search_results = re.findall(r'watch\?v=(\S{11})', html_content.read().decode()) # finds all links in search result
+                    webbrowser.open("http://www.youtube.com/watch?v={}".format(search_results[0]))
+                    pass
             
-            response = response + ' ' + 'It is ' + str(hour) + ':' + minute + ' ' + meridiem + '. ' 
+            # Search for terms on Wikipedia
+            elif ('search wikipedia for' in text):
+                assistantResponse("Searching through wikipedia ")
+            
+                try: 
+                    reg_ex = re.search('search wikipedia for (.*)', text)
+                    if (reg_ex):
+                        wiki_search = text.split("wikipedia for ", 1)[1] # Stores search keyword from the user
+                        # For debugging
+                        # print(wiki_search)
 
+                        wiki = wikipedia.summary(wiki_search, sentences = 3)
+                        assistantResponse(wiki)
+                except: 
+                    assistantResponse("The term could not be found on Wikipedia")
 
-        # Check to see if the user wants to know about a person
-        elif ('who is' in text):
-            person = getPerson(text)
-            wiki = wikipedia.summary(person, sentences = 2)
-            response = response + ' ' + wiki
+            # Retrieve latest news feeds
+            elif ('news for today') in text:
+                try:
+                    news_url = "https://news.google.com/news/rss"
+                    news_list = getRSSHeadlines(news_url)
+                    
+                    for news in news_list[:15]:
+                        assistantResponse(news.title.text)
+                except Exception as e:
+                    print(e)
 
-        # Search Google
-        elif 'open google and search' in text:
-            reg_ex = re.search('open google and search (.*)', text)
-            search_for = text.split("search", 1)[1] # Stores search keyword from the user
-            url = 'https://www.google.com/'
-            # if reg_ex:
-            #     subgoogle = reg_ex.group(1)
-            #     url = url + 'r/' + subgoogle
-            # print('Okay!')
-
-            driver = webdriver.Chrome("C:/Users/cogbo2/Computer Programs/Python/chromedriver.exe")
-            driver.get('http://www.google.com')
-            search = driver.find_element_by_name('q') # Finds search box element
-            search.send_keys(str(search_for)) # Sends search keys
-            search.send_keys(Keys.RETURN) # Hits enter
-
-        elif 'what is' in text:
-            reg_ex = re.search('what is (.*)', text)
-            search_for = text.split("is", 1)[1] # Stores search keyword from the user
-            url = 'https://www.google.com/'
-            # if reg_ex:
-            #     subgoogle = reg_ex.group(1)
-            #     url = url + 'r/' + subgoogle
-            # print('Okay!')
-
-            driver = webdriver.Chrome("C:/Users/cogbo2/Computer Programs/Python/chromedriver.exe")
-            driver.get('http://www.google.com')
-            search = driver.find_element_by_name('q') # Finds search box element
-            search.send_keys(str(search_for)) # Sends search keys
-            search.send_keys(Keys.RETURN) # Hits enter
-        
-        elif ('youtube' in text):
-            assistantResponse("Ok!")
-            reg_ex = re.search('youtube (.+)', text)
-            if (reg_ex):
-                domain = text.split("youtube", 1)[1]
-                query_string = urllib.parse.urlencode({"search_query" : domain})
-                html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string) 
-                search_results = re.findall(r'watch\?v=(\S{11})', html_content.read().decode()) # finds all links in search result
-                webbrowser.open("http://www.youtube.com/watch?v={}".format(search_results[0]))
-                pass
-
-
-        if (len(response) > 0):
-            # Assistant Audio Response 
-            assistantResponse(response)
-        else:
-            continue
-        
-        
-
+            
